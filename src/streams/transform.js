d20d4dfdbecc47575b88be586864e14f5291273b
olga.aheichyk <olga.aheichyk@gmail.com>
2024-10-07 20:42:38 +0200

    feat: implement streams subtasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'stream';
+
+class ReverseStream extends Transform {
+    constructor() {
+        super();
+    }
+
+    _transform(chunk, encoding, callback) {
+        const reversedChunk = chunk.toString().split("").reverse().join("");
+        this.push(reversedChunk);
+        callback();
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new ReverseStream();
+
+    process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
