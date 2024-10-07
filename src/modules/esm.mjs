d970917446805ca56975bf6c04adc2aa37b4467f
olga.aheichyk <olga.aheichyk@gmail.com>
2024-10-07 16:29:13 +0200

    feat: realize rewriting common js code to ECMAScript modules code

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,20 +1,24 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+
+const __filename = import.meta.filename;
+const __dirname = import.meta.dirname;
+
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = await import("./files/a.json", { with: { type: 'json' } });
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = await import("./files/b.json", { with: { type: "json" } });
 }
 
-console.log(`Release ${release()}`);
-console.log(`Version ${version()}`);
+console.log(`Release ${release}`);
+console.log(`Version ${version}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
 console.log(`Path to current file is ${__filename}`);
@@ -33,7 +37,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
