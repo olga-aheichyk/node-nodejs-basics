f1d00a32613252be6a5a0f95c46c90c7bb4ba128
olga.aheichyk <olga.aheichyk@gmail.com>
2024-10-08 16:34:11 +0200

    feat: implement zlib subtasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,32 @@
+import path from "path";
+import fs from "fs";
+import zlib from 'zlib';
+const dirname = import.meta.dirname;
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = path.resolve(dirname, './files/fileToCompress.txt');
+    const compressedFilePath = path.resolve(dirname, "./files/archive.gz");
+
+    const compressedFile = zlib.createGzip(filePath);
+
+    const readableStream = fs.createReadStream(filePath);
+    readableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+
+    const writableStream = fs.createWriteStream(compressedFilePath);
+    writableStream.on("error", (err) => {
+      console.error(err.message);
+    });
+
+    readableStream.pipe(compressedFile).pipe(writableStream);
+
+    writableStream.on("finish", () => {
+      console.log(
+        "Compressed data from fileToCompress.txt has been written to archive.gz"
+      );
+    });
+
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,32 @@
+import path from "path";
+import fs from "fs";
+import zlib from "zlib";
+const dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+  const filePath = path.resolve(dirname, "./files/archive.gz");
+  const decompressedFilePath = path.resolve(dirname, "./files/fileToCompress.txt"
+  );
+
+  const decompressedFile = zlib.createGunzip(filePath);
+
+  const readableStream = fs.createReadStream(filePath);
+  readableStream.on("error", (err) => {
+    console.error(err.message);
+  });
+
+  const writableStream = fs.createWriteStream(decompressedFilePath);
+  writableStream.on("error", (err) => {
+    console.error(err.message);
+  });
+
+  readableStream.pipe(decompressedFile).pipe(writableStream);
+
+  writableStream.on("finish", () => {
+    console.log(
+      "Decompressed data from archive.gz has been written to fileToCompress.txt"
+    );
+  });
 };
 
 await decompress();
